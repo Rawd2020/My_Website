de4ceb173fdeff1a905e4e4cd4eaf065e61f64d3
Rory Williams Doyle <rawd2020personal@gmail.com>
2017-06-08 21:08:53 +0100

    Implemented Google Recaptcha.

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
-
+$field_recaptcha = $_POST['g-recaptcha-response'];
 $mail_to = 'rorywilliamsdoyle@outlook.com';
 $subject = 'Message from a site visitor '.$field_name;
 
@@ -13,20 +13,27 @@ $body_message .= 'Message: '.$field_message;
 $headers = 'From: '.$field_email."\r\n";
 $headers .= 'Reply-To: '.$field_email."\r\n";
 
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
-
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. I will contact you as soon as possible.');
-		window.location = 'contact.html';
-	</script>
-<?php
-}
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to rorywilliamsdoyle@outlook.com');
-		window.location = 'contact.html';
-	</script>
-<?php
-}
-?>
+if ($field_recaptcha) {
+    $mail_status = mail($mail_to, $subject, $body_message, $headers);
+    if ($mail_status) { ?>
+        <script language="javascript" type="text/javascript">
+            alert('Thank you for the message. I will contact you as soon as possible.');
+            window.location = 'contact.html';
+        </script>
+    <?php
+    }
+    else { ?>
+        <script language="javascript" type="text/javascript">
+            alert('Message failed. Please, send an email to rorywilliamsdoyle@outlook.com');
+            window.location = 'contact.html';
+        </script>
+    <?php
+    }}
+     else { ?>
+        <script language="javascript" type="text/javascript">
+            alert('Message failed. Please, send an email to rorywilliamsdoyle@outlook.com');
+            window.location = 'contact.html';
+        </script>
+    <?php
+     }
+    ?>
